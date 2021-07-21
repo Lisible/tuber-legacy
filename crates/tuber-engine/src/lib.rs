c0a6a100c406c48446eea706eacaa352ad8ba309
Clément Sibille <clement.sibille@protonmail.com>
2021-07-21 21:05:24 +0200

    tuber-engine: Make it so that only the current state is rendered

--- crates/tuber-engine/src/lib.rs
+++ crates/tuber-engine/src/lib.rs
@@ -60,87 +60,87 @@ impl Engine {
 
     pub fn render(&mut self) {
         let graphics = self.graphics.as_mut().unwrap();
-        for state_context in self.state_stack.state_contexts_mut() {
-            let (camera_id, (camera, _, camera_transform)) = state_context
+        let state_context = self
+            .state_stack
+            .state_contexts_mut()
+            .last_mut()
+            .expect("There is no state");
+
+        let (camera_id, (camera, _, camera_transform)) = state_context
+            .ecs
+            .query_one::<(R<OrthographicCamera>, R<Active>, R<Transform2D>)>()
+            .expect("There is no camera");
+        graphics.update_camera(camera_id, &camera, &camera_transform);
+
+        for (_, (tilemap, tilemap_render, transform)) in
+            state_context
                 .ecs
-                .query_one::<(R<OrthographicCamera>, R<Active>, R<Transform2D>)>()
-                .expect("There is no camera");
-            graphics.update_camera(camera_id, &camera, &camera_transform);
-
-            for (_, (tilemap, tilemap_render, transform)) in
-                state_context
-                    .ecs
-                    .query::<(R<Tilemap>, R<TilemapRender>, R<Transform2D>)>()
-            {
-                graphics.prepare_tilemap(&tilemap, &tilemap_render, &transform);
-            }
-
-            for (_, (rectangle_shape, transform)) in state_context
-                .ecs
-                .query::<(R<RectangleShape>, R<Transform2D>)>()
-            {
-                graphics.prepare_rectangle(&rectangle_shape, &transform, true);
-            }
-            for (_, (sprite, transform)) in state_context.ecs.query::<(R<Sprite>, R<Transform2D>)>()
-            {
-                graphics.prepare_sprite(&sprite, &transform, true).unwrap();
-            }
-            for (_, (animated_sprite, transform)) in state_context
-                .ecs
-                .query::<(R<AnimatedSprite>, R<Transform2D>)>()
-            {
-                graphics
-                    .prepare_animated_sprite(&animated_sprite, &transform, true)
-                    .unwrap();
-            }
-
-            for (_, (mut tilemap_render,)) in state_context.ecs.query::<(W<TilemapRender>,)>() {
-                tilemap_render.dirty = false;
-            }
-
-            for (id, (frame, transform)) in state_context.ecs.query::<(R<Frame>, R<Transform2D>)>()
-            {
-                let apply_view_transform = !state_context
-                    .ecs
-                    .query_one_by_id::<(R<NoViewTransform>,)>(id)
-                    .is_some();
-                graphics.prepare_rectangle(
-                    &RectangleShape {
-                        width: frame.width,
-                        height: frame.height,
-                        color: frame.color,
-                    },
-                    &transform,
-                    apply_view_transform,
-                );
-            }
-
-            for (id, (text, transform)) in state_context.ecs.query::<(R<Text>, R<Transform2D>)>() {
-                let apply_view_transform = !state_context
-                    .ecs
-                    .query_one_by_id::<(R<NoViewTransform>,)>(id)
-                    .is_some();
-                graphics.prepare_text(text.text(), text.font(), &transform, apply_view_transform);
-            }
-
-            for (id, (image, transform)) in state_context.ecs.query::<(R<Image>, R<Transform2D>)>()
-            {
-                let apply_view_transform = !state_context
-                    .ecs
-                    .query_one_by_id::<(R<NoViewTransform>,)>(id)
-                    .is_some();
-                let sprite = Sprite {
-                    width: image.width,
-                    height: image.height,
-                    texture: image.texture.clone(),
-                };
-
-                graphics
-                    .prepare_sprite(&sprite, &transform, apply_view_transform)
-                    .unwrap();
-            }
+                .query::<(R<Tilemap>, R<TilemapRender>, R<Transform2D>)>()
+        {
+            graphics.prepare_tilemap(&tilemap, &tilemap_render, &transform);
         }
 
+        for (_, (rectangle_shape, transform)) in state_context
+            .ecs
+            .query::<(R<RectangleShape>, R<Transform2D>)>()
+        {
+            graphics.prepare_rectangle(&rectangle_shape, &transform, true);
+        }
+        for (_, (sprite, transform)) in state_context.ecs.query::<(R<Sprite>, R<Transform2D>)>() {
+            graphics.prepare_sprite(&sprite, &transform, true).unwrap();
+        }
+        for (_, (animated_sprite, transform)) in state_context
+            .ecs
+            .query::<(R<AnimatedSprite>, R<Transform2D>)>()
+        {
+            graphics
+                .prepare_animated_sprite(&animated_sprite, &transform, true)
+                .unwrap();
+        }
+
+        for (_, (mut tilemap_render,)) in state_context.ecs.query::<(W<TilemapRender>,)>() {
+            tilemap_render.dirty = false;
+        }
+
+        for (id, (frame, transform)) in state_context.ecs.query::<(R<Frame>, R<Transform2D>)>() {
+            let apply_view_transform = !state_context
+                .ecs
+                .query_one_by_id::<(R<NoViewTransform>,)>(id)
+                .is_some();
+            graphics.prepare_rectangle(
+                &RectangleShape {
+                    width: frame.width,
+                    height: frame.height,
+                    color: frame.color,
+                },
+                &transform,
+                apply_view_transform,
+            );
+        }
+
+        for (id, (text, transform)) in state_context.ecs.query::<(R<Text>, R<Transform2D>)>() {
+            let apply_view_transform = !state_context
+                .ecs
+                .query_one_by_id::<(R<NoViewTransform>,)>(id)
+                .is_some();
+            graphics.prepare_text(text.text(), text.font(), &transform, apply_view_transform);
+        }
+
+        for (id, (image, transform)) in state_context.ecs.query::<(R<Image>, R<Transform2D>)>() {
+            let apply_view_transform = !state_context
+                .ecs
+                .query_one_by_id::<(R<NoViewTransform>,)>(id)
+                .is_some();
+            let sprite = Sprite {
+                width: image.width,
+                height: image.height,
+                texture: image.texture.clone(),
+            };
+
+            graphics
+                .prepare_sprite(&sprite, &transform, apply_view_transform)
+                .unwrap();
+        }
         graphics.render();
     }
 }
