5b7b0d9f2478c7cac4320237f18bf5c74ae150db
Clément Sibille <claymeuns@protonmail.com>
2022-02-06 22:01:44 +0100

    escape-orcs-2: Add tilemap layers

--- examples/escape-orcs-2/terrain.rs
+++ examples/escape-orcs-2/terrain.rs
@@ -70,128 +70,37 @@ pub fn create_tilemap(asset_store: &mut AssetStore) -> Tilemap {
     let atlas = asset_store.asset::<TextureAtlas>("atlas").unwrap();
 
     let mut tilemap = Tilemap::new(
-        Size2::new(10, 10),
+        Size2::new(30, 30),
         Size2::new(TILE_SIZE, TILE_SIZE),
         MaterialDescriptor {
             albedo_map: "spritesheet".to_string(),
             normal_map: Some("normal_spritesheet".to_string()),
             emission_map: Some("emissive_spritesheet".to_string()),
         },
-        Some(Tile::StaticTile(StaticTile {
-            texture_region: atlas.texture_region("stone").unwrap(),
-        })),
     );
 
-    for y in 0..WORLD_SIZE.height {
-        tilemap.set_tile(
-            0,
-            y,
-            Some(Tile::AnimatedTile(AnimatedTile {
-                animation_state: AnimationState {
-                    keyframes: vec![
-                        atlas.texture_region("cliff_west_1").unwrap(),
-                        atlas.texture_region("cliff_west_2").unwrap(),
-                    ],
-                    ..Default::default()
-                },
-            })),
-        );
-        tilemap.set_tile(
-            WORLD_SIZE.width - 1,
-            y,
-            Some(Tile::AnimatedTile(AnimatedTile {
-                animation_state: AnimationState {
-                    keyframes: vec![
-                        atlas.texture_region("cliff_east_1").unwrap(),
-                        atlas.texture_region("cliff_east_2").unwrap(),
-                    ],
-                    ..Default::default()
-                },
-            })),
-        );
+    let _background_layer = tilemap.add_layer(Some(Tile::AnimatedTile(AnimatedTile {
+        animation_state: AnimationState {
+            keyframes: vec![
+                atlas.texture_region("lava_1").unwrap(),
+                atlas.texture_region("lava_2").unwrap(),
+            ],
+            ..Default::default()
+        },
+    })));
+
+    let terrain_layer = tilemap.add_layer(None);
+    for y in 0..5 {
+        for x in 0..5 {
+            tilemap.layer_mut(terrain_layer).unwrap().set_tile(
+                10 + x,
+                5 + y,
+                Some(Tile::StaticTile(StaticTile {
+                    texture_region: atlas.texture_region("stone").unwrap(),
+                })),
+            );
+        }
     }
-
-    for x in 0..WORLD_SIZE.width {
-        tilemap.set_tile(
-            x,
-            0,
-            Some(Tile::AnimatedTile(AnimatedTile {
-                animation_state: AnimationState {
-                    keyframes: vec![
-                        atlas.texture_region("cliff_north_1").unwrap(),
-                        atlas.texture_region("cliff_north_2").unwrap(),
-                    ],
-                    ..Default::default()
-                },
-            })),
-        );
-        tilemap.set_tile(
-            x,
-            WORLD_SIZE.height - 1,
-            Some(Tile::AnimatedTile(AnimatedTile {
-                animation_state: AnimationState {
-                    keyframes: vec![
-                        atlas.texture_region("cliff_south_1").unwrap(),
-                        atlas.texture_region("cliff_south_2").unwrap(),
-                    ],
-                    ..Default::default()
-                },
-            })),
-        );
-    }
-
-    tilemap.set_tile(
-        0,
-        0,
-        Some(Tile::AnimatedTile(AnimatedTile {
-            animation_state: AnimationState {
-                keyframes: vec![
-                    atlas.texture_region("corner_north_west_1").unwrap(),
-                    atlas.texture_region("corner_north_west_2").unwrap(),
-                ],
-                ..Default::default()
-            },
-        })),
-    );
-    tilemap.set_tile(
-        WORLD_SIZE.width - 1,
-        0,
-        Some(Tile::AnimatedTile(AnimatedTile {
-            animation_state: AnimationState {
-                keyframes: vec![
-                    atlas.texture_region("corner_north_east_1").unwrap(),
-                    atlas.texture_region("corner_north_east_2").unwrap(),
-                ],
-                ..Default::default()
-            },
-        })),
-    );
-    tilemap.set_tile(
-        0,
-        WORLD_SIZE.height - 1,
-        Some(Tile::AnimatedTile(AnimatedTile {
-            animation_state: AnimationState {
-                keyframes: vec![
-                    atlas.texture_region("corner_south_west_1").unwrap(),
-                    atlas.texture_region("corner_south_west_2").unwrap(),
-                ],
-                ..Default::default()
-            },
-        })),
-    );
-    tilemap.set_tile(
-        WORLD_SIZE.width - 1,
-        WORLD_SIZE.height - 1,
-        Some(Tile::AnimatedTile(AnimatedTile {
-            animation_state: AnimationState {
-                keyframes: vec![
-                    atlas.texture_region("corner_south_east_1").unwrap(),
-                    atlas.texture_region("corner_south_east_2").unwrap(),
-                ],
-                ..Default::default()
-            },
-        })),
-    );
 
     tilemap
 }
