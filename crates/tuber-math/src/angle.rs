0b86aa844031fcc3056d5d65bf8fae71fed1fb3f
Clément Sibille <claymeuns@protonmail.com>
2022-02-15 22:23:42 +0100

    math: Always store the angles value in radians

--- crates/tuber-math/src/angle.rs
+++ crates/tuber-math/src/angle.rs
@@ -3,27 +3,30 @@ use std::ops::{Div, Mul};
 use crate::number_traits::Pi;
 
 #[derive(Clone, Copy)]
-pub enum Angle<T> {
-    Radians(T),
-    Degrees(T),
+pub struct Angle<T> {
+    angle_radians: T,
 }
 
 impl<T> Angle<T>
 where
     T: Copy + Pi + Mul<Output = T> + Div<Output = T> + From<i32>,
 {
-    pub fn radians(&self) -> T {
-        match self {
-            Angle::Radians(angle_radians) => *angle_radians,
-            Angle::Degrees(angle_degrees) => *angle_degrees * T::pi() / T::from(180),
+    pub fn new_radians(angle_radians: T) -> Self {
+        Self { angle_radians }
+    }
+
+    pub fn new_degrees(angle_degrees: T) -> Self {
+        Self {
+            angle_radians: angle_degrees * T::pi() / T::from(180),
         }
     }
 
+    pub fn radians(&self) -> T {
+        self.angle_radians
+    }
+
     pub fn degrees(&self) -> T {
-        match self {
-            Angle::Radians(angle_radians) => *angle_radians * T::from(180) / T::pi(),
-            Angle::Degrees(angle_degrees) => *angle_degrees,
-        }
+        self.angle_radians * T::from(180) / T::pi()
     }
 }
 
@@ -33,7 +36,7 @@ mod tests {
 
     #[test]
     fn into_radians() {
-        let angle = Angle::Degrees(120.0);
+        let angle = Angle::new_degrees(120.0);
         let radians = angle.radians();
 
         assert_eq!(radians as i32, (2.0 * std::f64::consts::FRAC_PI_3) as i32);
@@ -41,7 +44,7 @@ mod tests {
 
     #[test]
     fn into_degrees() {
-        let angle = Angle::Radians(std::f64::consts::PI);
+        let angle = Angle::new_radians(std::f64::consts::PI);
         let degrees = angle.degrees();
 
         assert_eq!(degrees as i32, 180);
