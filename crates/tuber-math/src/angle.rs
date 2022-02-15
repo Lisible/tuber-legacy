bf119784ec66f5a116bcba9967d20a61d5b8c387
Clément Sibille <claymeuns@protonmail.com>
2022-02-15 22:14:27 +0100

    math: Replace IntoRadians,Degrees traits by methods on Angle struct

--- crates/tuber-math/src/angle.rs
+++ crates/tuber-math/src/angle.rs
@@ -10,44 +10,19 @@ pub enum Angle<T> {
 
 impl<T> Angle<T>
 where
-    T: Copy,
-{
-    pub fn value(&self) -> T {
-        match self {
-            Angle::Radians(value) => *value,
-            Angle::Degrees(value) => *value,
-        }
-    }
-}
-
-pub trait IntoDegrees<T> {
-    fn into_degrees(self) -> Angle<T>;
-}
-
-impl<T> IntoDegrees<T> for Angle<T>
-where
     T: Copy + Pi + Mul<Output = T> + Div<Output = T> + From<i32>,
 {
-    fn into_degrees(self) -> Angle<T> {
+    pub fn radians(&self) -> T {
         match self {
-            Angle::Radians(angle_radians) => Self::Degrees(angle_radians * T::from(180) / T::pi()),
-            Angle::Degrees(_) => self,
+            Angle::Radians(angle_radians) => *angle_radians,
+            Angle::Degrees(angle_degrees) => *angle_degrees * T::pi() / T::from(180),
         }
     }
-}
 
-pub trait IntoRadians<T> {
-    fn into_radians(self) -> Angle<T>;
-}
-
-impl<T> IntoRadians<T> for Angle<T>
-where
-    T: Copy + Pi + Mul<Output = T> + Div<Output = T> + From<i32>,
-{
-    fn into_radians(self) -> Angle<T> {
+    pub fn degrees(&self) -> T {
         match self {
-            Angle::Radians(_) => self,
-            Angle::Degrees(angle_degrees) => Self::Radians(angle_degrees * T::pi() / T::from(180)),
+            Angle::Radians(angle_radians) => *angle_radians * T::from(180) / T::pi(),
+            Angle::Degrees(angle_degrees) => *angle_degrees,
         }
     }
 }
@@ -59,7 +34,7 @@ mod tests {
     #[test]
     fn into_radians() {
         let angle = Angle::Degrees(120.0);
-        let radians = angle.into_radians().value();
+        let radians = angle.radians();
 
         assert_eq!(radians as i32, (2.0 * std::f64::consts::FRAC_PI_3) as i32);
     }
@@ -67,7 +42,7 @@ mod tests {
     #[test]
     fn into_degrees() {
         let angle = Angle::Radians(std::f64::consts::PI);
-        let degrees = angle.into_degrees().value();
+        let degrees = angle.degrees();
 
         assert_eq!(degrees as i32, 180);
     }
