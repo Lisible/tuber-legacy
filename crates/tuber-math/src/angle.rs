9adf7e6343a07de55959fea108f61a6d60b03eea
Clément Sibille <claymeuns@protonmail.com>
2022-02-15 22:04:02 +0100

    math: Implement IntoDegrees,Radians for all float types

--- crates/tuber-math/src/angle.rs
+++ crates/tuber-math/src/angle.rs
@@ -1,3 +1,7 @@
+use std::ops::{Div, Mul};
+
+use crate::number_traits::Pi;
+
 #[derive(Clone, Copy)]
 pub enum Angle<T> {
     Radians(T),
@@ -20,13 +24,14 @@ pub trait IntoDegrees<T> {
     fn into_degrees(self) -> Angle<T>;
 }
 
-impl IntoDegrees<f32> for Angle<f32> {
-    fn into_degrees(self) -> Angle<f32> {
+impl<T> IntoDegrees<T> for Angle<T>
+where
+    T: Copy + Pi + Mul<Output = T> + Div<Output = T> + From<i32>,
+{
+    fn into_degrees(self) -> Angle<T> {
         match self {
-            Angle::Radians(_) => self,
-            Angle::Degrees(angle_degrees) => {
-                Self::Radians(angle_degrees * std::f32::consts::PI / 180f32)
-            }
+            Angle::Radians(angle_radians) => Self::Degrees(angle_radians * T::from(180) / T::pi()),
+            Angle::Degrees(_) => self,
         }
     }
 }
@@ -35,13 +40,35 @@ pub trait IntoRadians<T> {
     fn into_radians(self) -> Angle<T>;
 }
 
-impl IntoRadians<f32> for Angle<f32> {
-    fn into_radians(self) -> Angle<f32> {
+impl<T> IntoRadians<T> for Angle<T>
+where
+    T: Copy + Pi + Mul<Output = T> + Div<Output = T> + From<i32>,
+{
+    fn into_radians(self) -> Angle<T> {
         match self {
-            Angle::Radians(angle_radians) => {
-                Self::Degrees(angle_radians * 180f32 / std::f32::consts::PI)
-            }
-            Angle::Degrees(_) => self,
+            Angle::Radians(_) => self,
+            Angle::Degrees(angle_degrees) => Self::Radians(angle_degrees * T::pi() / T::from(180)),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn into_radians() {
+        let angle = Angle::Degrees(120.0);
+        let radians = angle.into_radians().value();
+
+        assert_eq!(radians as i32, (2.0 * std::f64::consts::FRAC_PI_3) as i32);
+    }
+
+    #[test]
+    fn into_degrees() {
+        let angle = Angle::Radians(std::f64::consts::PI);
+        let degrees = angle.into_degrees().value();
+
+        assert_eq!(degrees as i32, 180);
     }
 }
--- crates/tuber-math/src/number_traits.rs
+++ crates/tuber-math/src/number_traits.rs
@@ -29,3 +29,19 @@ impl Zero for f32 {
         0.0
     }
 }
+
+pub trait Pi {
+    fn pi() -> Self;
+}
+
+impl Pi for f32 {
+    fn pi() -> Self {
+        std::f32::consts::PI
+    }
+}
+
+impl Pi for f64 {
+    fn pi() -> Self {
+        std::f64::consts::PI
+    }
+}
