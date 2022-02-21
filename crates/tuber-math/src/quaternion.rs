6813c67dbec2534718e616c19fae38c72e43c5b1
Clément Sibille <claymeuns@protonmail.com>
2022-02-21 21:12:21 +0100

    math: Add normalization functions to quaternions

--- crates/tuber-math/src/quaternion.rs
+++ crates/tuber-math/src/quaternion.rs
@@ -5,18 +5,18 @@ use crate::matrix::Matrix4;
 use crate::number_traits::Float;
 use crate::vector::Vector3;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Quaternion<T>
-    where
-        T: Debug,
+where
+    T: Debug,
 {
     scalar_part: T,
     vector_part: Vector3<T>,
 }
 
 impl<T> Quaternion<T>
-    where
-        T: Debug + Float,
+where
+    T: Debug + Float,
 {
     pub fn new(scalar_part: T, vector_part: Vector3<T>) -> Self {
         Self {
@@ -54,11 +54,36 @@ impl<T> Quaternion<T>
             T::zero(), T::zero(), T::zero(), T::one()
         ])
     }
+
+    pub fn normalize(&mut self) {
+        let norm = self.norm();
+        self.vector_part /= norm;
+        self.scalar_part /= norm;
+    }
+
+    pub fn normalized(&self) -> Self {
+        let mut normalized_quaternion = self.clone();
+        normalized_quaternion.normalize();
+        normalized_quaternion
+    }
+
+    pub fn norm(&self) -> T {
+        let x = self.vector_part.x();
+        let y = self.vector_part.y();
+        let z = self.vector_part.z();
+        let w = self.scalar_part;
+        let xx = x * x;
+        let yy = y * y;
+        let zz = z * z;
+        let ww = w * w;
+
+        (ww + xx + yy + zz).sqrt()
+    }
 }
 
 impl<T> Display for Quaternion<T>
-    where
-        T: Debug + Float,
+where
+    T: Debug + Float,
 {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         writeln!(
@@ -73,8 +98,8 @@ impl<T> Display for Quaternion<T>
 }
 
 impl<T> Mul for Quaternion<T>
-    where
-        T: Debug + Float,
+where
+    T: Debug + Float,
 {
     type Output = Self;
 
@@ -141,5 +166,38 @@ mod tests {
         assert_float_absolute_eq!(matrix[3][1], 0.0, 0.02);
         assert_float_absolute_eq!(matrix[3][2], 0.0, 0.02);
         assert_float_absolute_eq!(matrix[3][3], 1.0, 0.02);
+    }
+
+    #[test]
+    fn norm() {
+        let quaternion = Quaternion::new(23.0, Vector3::new(12.0, 34.0, 56.0));
+
+        let norm = quaternion.norm();
+
+        assert_float_absolute_eq!(norm, 70.46, 0.01);
+    }
+
+    #[test]
+    fn normalize() {
+        let mut quaternion = Quaternion::new(23.0, Vector3::new(12.0, 34.0, 56.0));
+
+        quaternion.normalize();
+
+        assert_float_absolute_eq!(quaternion.scalar_part, 0.32, 0.01);
+        assert_float_absolute_eq!(quaternion.vector_part.x(), 0.17, 0.01);
+        assert_float_absolute_eq!(quaternion.vector_part.y(), 0.48, 0.01);
+        assert_float_absolute_eq!(quaternion.vector_part.z(), 0.79, 0.01);
+    }
+
+    #[test]
+    fn normalized() {
+        let quaternion = Quaternion::new(23.0, Vector3::new(12.0, 34.0, 56.0));
+
+        let normalized = quaternion.normalized();
+
+        assert_float_absolute_eq!(normalized.scalar_part, 0.32, 0.01);
+        assert_float_absolute_eq!(normalized.vector_part.x(), 0.17, 0.01);
+        assert_float_absolute_eq!(normalized.vector_part.y(), 0.48, 0.01);
+        assert_float_absolute_eq!(normalized.vector_part.z(), 0.79, 0.01);
     }
 }
