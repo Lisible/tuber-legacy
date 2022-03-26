7fe60cb0511e3fd43df59070e0ac4fe6d5562806
Clément Sibille <claymeuns@protonmail.com>
2022-03-26 10:32:14 +0100

    examples: Add rectangle example

--- examples/rectangle/main.rs
+++ examples/rectangle/main.rs
@@ -1,7 +1,12 @@
+use tuber::core::transform::Transform;
+use tuber::ecs::ecs::Ecs;
+use tuber::ecs::system::SystemBundle;
+use tuber::engine::engine_context::EngineContext;
 use tuber::engine::state::State;
 use tuber::engine::Engine;
 use tuber::engine::EngineSettings;
 use tuber::engine::TuberRunner;
+use tuber::graphics::renderable::rectangle_shape::RectangleShape;
 use tuber::WinitTuberRunner;
 
 fn main() {
@@ -14,4 +19,11 @@ fn main() {
 }
 
 struct MainState;
-impl State for MainState {}
+impl State for MainState {
+    fn render(&mut self, _ecs: &mut Ecs, engine_context: &mut EngineContext) { 
+       engine_context
+            .graphics
+            .draw_rectangle_shape(RectangleShape::new(1.0, 1.0))
+            .unwrap();
+    }
+}
